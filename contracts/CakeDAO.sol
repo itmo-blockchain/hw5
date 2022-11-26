98d5a454d8b193b213c38b4994a3b66525250e9b
Michael Ruzavin <misharuz25@gmail.com>
2022-11-26 23:13:27 +0300

    Add create proposal

--- contracts/CakeDAO.sol
+++ contracts/CakeDAO.sol
@@ -1,27 +1,76 @@
 // SPDX-License-Identifier: MIT
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 pragma solidity ^0.8.9;
 
 contract CakeDAO {
 
-    enum ProposalState { Active, Accepted, Discarded, Expired }
+    enum ProposalState { Unknown, Active, Accepted, Discarded, Expired }
+
     struct Proposal {
         ProposalState state;
         uint256 forVotes;
         uint256 againstVotes;
         uint256 expiration;
     }
+
+    uint8 public constant MAX_PROPOSALS = 3;
+    uint256 public constant VOTING_DURATION = 3 days;
         
-    address public token;
+    IERC20 public token;
     
-    mapping (bytes32 => Proposal) name;
-    bytes32[3] private proposals;
+    mapping (bytes32 => Proposal) proposals;
+
+    // Simple queue to keep track of the proposals   
+    bytes32[MAX_PROPOSALS] private currentProposals;
+    // The index of the next proposal to be added to currentProposals
+    uint8 private nextProposalNum = 0;
+
+    event NewProposal(bytes32 indexed proposalId, uint256 expiration);
+    event Vote(bytes32 indexed proposalId, address indexed voter, bool forVote);
+    event ProposalResult(bytes32 indexed proposalId, ProposalState state);
     
     constructor(address _token) {
-        token = _token;
+        token = IERC20(_token);
     }
 
     function createProposal(bytes32 _proposalHash) external {
+        require(
+            token.balanceOf(msg.sender) > 0,
+            "Not enough balance"
+        );
+
+        require(
+            proposals[_proposalHash].state == ProposalState.Unknown,
+            "Proposal already exists"
+        );
+
+        bytes32 oldProposal = currentProposals[nextProposalNum];
+
+        require(
+            oldProposal == bytes32(0) 
+            || proposals[oldProposal].state != ProposalState.Active
+            || proposals[oldProposal].expiration < block.timestamp,
+            "Already 3 active proposals"
+        );
+
+        if (oldProposal != bytes32(0)) {
+            proposals[oldProposal].state = ProposalState.Expired;
+            emit ProposalResult(oldProposal, ProposalState.Expired);
+        }    
+
+        proposals[_proposalHash] = Proposal({
+            state: ProposalState.Active,
+            forVotes: 0,
+            againstVotes: 0,
+            expiration: block.timestamp + VOTING_DURATION
+        });
+
+        currentProposals[nextProposalNum] = _proposalHash;  
+        nextProposalNum = (nextProposalNum + 1) % MAX_PROPOSALS;
+
+        emit NewProposal(_proposalHash, block.timestamp + VOTING_DURATION);
         
     }
 
@@ -29,23 +78,32 @@ contract CakeDAO {
         
     }
 
-    function executeProposal(bytes32 _proposalHash) external {
-        
-    }
-
     function getProposalState(bytes32 _proposalHash) external view returns (ProposalState) {
-        
+        return proposals[_proposalHash].state;
     }
 
     function getProposalVotes(bytes32 _proposalHash) external view returns (uint256, uint256) {
-        
+        return (proposals[_proposalHash].forVotes, proposals[_proposalHash].againstVotes);
     }
 
     function getProposalExpiration(bytes32 _proposalHash) external view returns (uint256) {
-        
+        return proposals[_proposalHash].expiration;        
     }
 
     function getProposals() external view returns (bytes32[3] memory) {
-        
+        return currentProposals;
+    }
+
+    function getCurrentProposalsCount() external view returns (uint8) {
+        uint8 count = 0;
+
+        for (uint8 i = 0; i < MAX_PROPOSALS; i++) {
+            if (proposals[currentProposals[i]].state == ProposalState.Active &&
+                proposals[currentProposals[i]].expiration > block.timestamp) {
+                count++;
+            }
+        }
+
+        return count;
     }
 }
--- test/CakeDAO.ts
+++ test/CakeDAO.ts
@@ -1,14 +1,127 @@
 import { ethers } from "hardhat";
+import { expect } from "chai";
+import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 
 describe("CakeDAO", function () {
-    async function deployTokenFixture() {
-        const [owner] = await ethers.getSigners();
-        const Cake = await ethers.getContractFactory("CakeToken");
+    async function deployDaoWith3BalancesFixture() {
+        const [alice, bob, charle, dan] = await ethers.getSigners();
 
+        const Cake = await ethers.getContractFactory("CakeToken");
         const cake = await Cake.deploy();
 
-        return { cake, owner };
+        const CakeDAO = await ethers.getContractFactory("CakeDAO");
+        const cakeDAO = await CakeDAO.deploy(cake.address);
+
+        await cake.connect(alice).transfer(bob.address, 40 * 10 ** 6);
+        await cake.connect(alice).transfer(charle.address, 35 * 10 ** 6);
+
+        return { cake, cakeDAO, alice, bob, charle, dan };
     }
 
-    
+    describe("Deployment", function () {
+        it("Should be right balances", async function () {
+            const { cake, alice, bob, charle } = await loadFixture(deployDaoWith3BalancesFixture);
+
+            expect(await cake.balanceOf(alice.address)).to.equal(25 * 10 ** 6);
+            expect(await cake.balanceOf(bob.address)).to.equal(40 * 10 ** 6);
+            expect(await cake.balanceOf(charle.address)).to.equal(35 * 10 ** 6);
+        });
+
+        it("Should be right totalSupply", async function () {
+            const { cake } = await loadFixture(deployDaoWith3BalancesFixture);
+
+            expect(await cake.totalSupply()).to.equal(100 * 10 ** 6);
+        });
+
+        it("Should be empty proposal list", async function () {
+            const { cakeDAO } = await loadFixture(deployDaoWith3BalancesFixture);
+
+            expect(await cakeDAO.getCurrentProposalsCount()).to.equal(0);
+        })
+    })
+
+    describe("Create proposal", function () {
+        it("Should be able to create a proposal", async function () {
+            const { cakeDAO, alice } = await loadFixture(deployDaoWith3BalancesFixture);
+
+            const prop = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("Proposal 1"));
+
+            await cakeDAO.connect(alice).createProposal(prop);
+
+            const [event] = await cakeDAO.queryFilter(cakeDAO.filters.NewProposal(null, null));
+
+            expect(event.args.proposalId).to.equal(prop);
+            expect(event.args.expiration).to.equal(await time.latest() + 3 * 24 * 60 * 60);
+
+            expect(await cakeDAO.getCurrentProposalsCount()).to.equal(1);
+
+            expect(await cakeDAO.getProposalExpiration(prop)).to.equal(await time.latest() + 3 * 24 * 60 * 60);
+            expect(await cakeDAO.getProposalState(prop)).to.equal(1);
+
+            (await cakeDAO.getProposalVotes(prop)).forEach(vote => {
+                expect(vote).to.equal(0);
+            })
+        });
+
+        it("Shouldn't be able to create a proposal if already exists", async function () {
+            const { cakeDAO, alice } = await loadFixture(deployDaoWith3BalancesFixture);
+
+            const prop = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("Proposal 1"));
+
+            await cakeDAO.connect(alice).createProposal(prop);
+
+            await expect(cakeDAO.connect(alice).createProposal(prop)).to.be.revertedWith("Proposal already exists");
+        });
+
+        it("Shouldn't be able to create a proposal if not enough balance", async function () {
+            const { cakeDAO, dan } = await loadFixture(deployDaoWith3BalancesFixture);
+
+            const prop = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("Proposal 1"));
+
+            await expect(cakeDAO.connect(dan).createProposal(prop)).to.be.revertedWith("Not enough balance");
+        });
+
+        it("Shouldn't be able to create a proposal if 3 proposals already exists", async function () {
+            const { cakeDAO, alice, bob, charle } = await loadFixture(deployDaoWith3BalancesFixture);
+
+            await cakeDAO.connect(alice).createProposal(ethers.utils.keccak256(ethers.utils.toUtf8Bytes("Proposal 1")));
+            await cakeDAO.connect(bob).createProposal(ethers.utils.keccak256(ethers.utils.toUtf8Bytes("Proposal 2")));
+            await cakeDAO.connect(charle).createProposal(ethers.utils.keccak256(ethers.utils.toUtf8Bytes("Proposal 3")));
+
+            await expect(
+                cakeDAO.connect(alice).createProposal(ethers.utils.keccak256(ethers.utils.toUtf8Bytes("Proposal 4")))
+                ).to.be.revertedWith("Already 3 active proposals");
+        });
+
+        it("Should be able to create a proposal if 3 proposals already exists and one is expired", async function () {
+            const { cakeDAO, alice, bob, charle } = await loadFixture(deployDaoWith3BalancesFixture);
+
+            await cakeDAO.connect(alice).createProposal(ethers.utils.keccak256(ethers.utils.toUtf8Bytes("Proposal 1")));
+            await time.increase(24 * 60 * 60);
+
+            await cakeDAO.connect(bob).createProposal(ethers.utils.keccak256(ethers.utils.toUtf8Bytes("Proposal 2")));
+            await time.increase(24 * 60 * 60);
+
+            await cakeDAO.connect(charle).createProposal(ethers.utils.keccak256(ethers.utils.toUtf8Bytes("Proposal 3")));
+            await time.increase(24 * 60 * 60);
+
+            await cakeDAO.connect(alice).createProposal(ethers.utils.keccak256(ethers.utils.toUtf8Bytes("Proposal 4")));
+
+            const [_fst, _snd, _thd, event] = await cakeDAO.queryFilter(cakeDAO.filters.NewProposal(null, null));
+
+            expect(event.args.proposalId).to.equal(ethers.utils.keccak256(ethers.utils.toUtf8Bytes("Proposal 4")));
+
+            expect(await cakeDAO.getCurrentProposalsCount()).to.equal(3);
+
+            const [realesd] = await cakeDAO.queryFilter(cakeDAO.filters.ProposalResult(null, null));
+
+            expect(realesd.args.proposalId).to.equal(ethers.utils.keccak256(ethers.utils.toUtf8Bytes("Proposal 1")));
+            expect(realesd.args.state).to.equal(4);
+        });
+    });
+
+    describe("Vote", function () {
+    });
+
+
 });
